49c38558a0f39cd3af2270f3f99aae24f7dfb976
Maurice <maurice.baez1@gmail.com>
2022-04-04 12:10:03 -0700

    added cupcake model and cupcakes route

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, redirect, render_template, flash, jsonify
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+
+
+@app.get("/api/cupcakes")
+def get_all_cupcakes():
+    """ Get data about all cupcakes """
+
+    cupcakes = Cupcake.query.all()
+    serialize = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialize)
--- models.py
+++ models.py
@@ -1,1 +1,54 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMG_URL = "https://tinyurl.com/demo-cupcake"
+
+class Cupcake(db.Model):
+    """ Cupcake """
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    flavor = db.Column(db.String(50),
+                     nullable=False)
+    size = db.Column(db.String(50),
+                        nullable=False)
+    rating = db.Column(db.Integer,
+                        nullable=False)
+    image = db.Column(db.String,
+                        nullable=False,
+                        default=DEFAULT_IMG_URL)
+
+
+    def __repr__(self):
+        """ Return cupcake data """
+
+        c = self
+
+        return f"<Id {c.id} Flavor {c.flavor} Size {c.size}> Rating {c.rating}"
+
+
+    def serialize(self):
+        """ Serialize to dictionary """
+
+        return {
+                "id": self.id,
+                "flavor": self.flavor,
+                "size": self.size,
+                "rating": self.rating,
+                "image": self.image
+        }
+
+
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
