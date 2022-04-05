480a533ec9f21290e38f729b1922fce8cf28f508
Maurice <maurice.baez1@gmail.com>
2022-04-05 08:02:27 -0700

    added delete cupcake functionality

--- static/api-calls.js
+++ static/api-calls.js
@@ -1,7 +1,9 @@
 "use strict"
 
-const BASE_API_URL = "http://127.0.0.1:5001/api"
+const BASE_API_URL = "http://localhost:5001/api"
 
+
+/** Get all cupcakes in the database */
 async function getCupcakes(){
   console.debug("getCupcakes");
   const response = await axios.get(`${BASE_API_URL}/cupcakes`);
@@ -9,18 +11,32 @@ async function getCupcakes(){
   return response.data.cupcakes;
 }
 
+/** Create a new cupcake in the database */
+async function new_cupcake() {
+  let flavor = $("#flavor").val();
+  let rating = $("#rating").val();
+  let size = $("#size").val();
+  let image = $("#image-url").val();
 
-async function new_cupcakes() {
-  let flavor = $("#flavor").val(); 
-  let rating = $("#rating").val(); 
-  let size = $("#size").val(); 
-  let image = $("#image-url").val(); 
-  const response = await axios.post(`${BASE_API_URL}/cupcakes`,
-                                    {flavor,
+  await axios.post(`${BASE_API_URL}/cupcakes`,
+                                    {
+                                      flavor,
                                       size,
                                       rating : Number(rating),
-                                      image});
-console.log(response);
+                                      image
+                                    });
+}
+
+/** Delete a cupcake from the database and clear from the DOM */
+async function delete_cupcake(evt){
+
+  const $cupcake = $(evt.target).closest(".cc-item");
+  const $cupcakeId = $(evt.target).closest(".cc").data("cc-id");
+
+  console.log("cupcake")
+  const response = await axios.delete(`${BASE_API_URL}/cupcakes/${$cupcakeId}`);
+
+  $cupcake.remove()
 }
 
 
--- static/ui.js
+++ static/ui.js
@@ -2,43 +2,55 @@
 
 const $cupcakeList = $("#cupcakes-list");
 const $form = $("#cupcake-form");
+const $deletebtn = $(".delete-cc");
 
+
+/** Generate HTML markup for each cupcake  */
 function generateCupcakeHTML(cupcake){
 
     const $cupcake = $("<li>");
-  
-    $cupcake.html(`<div class="row">
-                   <img src="${cupcake.image}" class="col-6 cupcake-img">
-  
-                   <span class="col-2">${cupcake.flavor}</span>
-                   <span class="col-2">${cupcake.rating}</span>
-                   <span class="col-2">${cupcake.size}</span>
+    $cupcake.addClass("list-group-item cc-item");
+
+    $cupcake.html(`<div data-cc-id="${cupcake.id}"
+                  class="d-flex justify-content-around align-items-center cc">
+                  <img src="${cupcake.image}" class="cupcake-img"> Flavor:${cupcake.flavor} Rating: ${cupcake.rating} Size: ${cupcake.size}
+                  <button class="delete-cc btn btn-outline-danger">Delete Cupcake</button>
                   </div>`
-                   );
-  
+                  );
+
     return $cupcake;
   }
-  
-  
-  
-  
-async function displayCupcakes() {
+
+
+/** Given a list of cupcakes, generate HTML and append each to the DOM  */
+async function displayCupcakes(cupcakes) {
     console.debug("displayCupcakes");
-  
-    const cupcakes = await getCupcakes();
-  
-    for(let cupcake of cupcakes){
-  
-     let cc = generateCupcakeHTML(cupcake);
-  
+
+    cupcakes.forEach(c => {
+      const cc = generateCupcakeHTML(c);
       $cupcakeList.append(cc);
-    }
+    })
   }
-  
-displayCupcakes();
+
+
+/** Retrieve cupcake list from database and display on page */
+async function getAndDisplayCupcakes(){
+    $cupcakeList.empty();
+    const cupcakes = await getCupcakes();
+    displayCupcakes(cupcakes);
+}
+
+
+// Get cupcake data and display on page load
+getAndDisplayCupcakes();
+
+
 
 $form.on("submit", async function (evt) {
     evt.preventDefault();
-    await new_cupcakes();
-    await displayCupcakes();
+    await new_cupcake();
+    await getAndDisplayCupcakes();
+    $form.trigger("reset")
 });
+
+$cupcakeList.on("click", ".delete-cc", delete_cupcake)
